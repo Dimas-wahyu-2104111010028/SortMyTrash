050764ceb7c144950d968cd4fa2c4e02f9d9930d
Arvin <arvin@mhs.unsyiah.ac.id>
2023-05-07 01:55:40 +0700

    test slicing beranda

--- front-end/src/components/atoms/index.js
+++ front-end/src/components/atoms/index.js
@@ -1,1 +1,4 @@
-export * from '..atoms'
+import Button from "./button/button.js";
+import Gap from "./gap/gap.js";
+
+export { Button, Gap }
--- front-end/src/pages/Beranda/Beranda.js
+++ front-end/src/pages/Beranda/Beranda.js
@@ -1,39 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
 import logo from '../../assets/img/logo.png'
 import edukasi from '../../assets/img/edukasi.svg'
 import pengaduan from '../../assets/img/pengaduan.svg'
 import riwayat from '../../assets/img/riwayat.svg'
 import sortir from '../../assets/img/sortir.svg'
 import Navbar from '../../components/layouts/Navbar'
-import css from './beranda.css'
+import './beranda.css'
+import { Button, Gap } from '../../components'
 
 function Beranda() {
     return (
         <>
-        <Navbar />
-        <div>
-            <div class="container mt-5">
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <p>
-                            <img src={logo} alt='logo' width="200px" />
-                        </p>
-                    </div>
-                </div>
-                <div class="row mt-5 justify-content-center">
-                    <div class="col-md-6 col-sm-12 text-center">
-                        <a href="/sortir" class="btn btn-lg btn-home mb-3 d-block"
-                        ><img src={sortir} width="60" alt="Sortir Sampah" /> Sortir Sampah</a>
-                        <a href="/pengaduan" class="btn btn-lg btn-home mb-3 d-block"
-                        ><img src={pengaduan} width="60" alt="Pengaduan" /> Pengaduan</a>
-                        <a href="/edukasiSampah" class="btn btn-lg btn-home mb-3 d-block"
-                        ><img src={edukasi} width="60" alt="Edukasi Sampah" /> Edukasi Sampah</a>
-                        <a href="/riwayat" class="btn btn-lg btn-home mb-3 d-block">
-                            <img src={riwayat} width="60" alt="Riwayat" /> Riwayat</a>
+            <Navbar />
+            <div>
+                <div className="container">
+                    <div className="sortir-container flex aic">
+                        <div className='card flex column aic jcc'>
+                            <div className='card-image'>
+                                <img className='img' src={sortir} alt='sortir' />
+                            </div>
+                            <Gap height="10"/>
+                            <div className='card-description'>
+                                <Button title="SORTIR" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        
         </>
     )
 }
