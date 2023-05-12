a25f277de7744a0e112a45a47c9e21dffca2a027
Navisah <zuriaton@mhs.unsyiah.ac.id>
2023-05-12 18:55:48 +0700

    Halaman Rincian Pengaduan

---
+++ halaman laporan petugas/rincian_pengaduan.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@200&family=Montserrat:wght@400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+    <title>Sortmytrash</title>
+    <link rel="icon" href="../assets/logo.png" type="image/x-icon" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+    />
+    <link rel="stylesheet" href="style_rincian.css" />
+  </head>
+
+  <body>
+    <nav class="navbar bg-success">
+      <div class="navbar mx-3 fs-5">
+        <a href=""><i class="bi bi-arrow-left" style="color: black"></i></a>
+      </div>
+      <div class="mx-auto text-center">
+        <p class="m-1">Laporan</p>
+      </div>
+    </nav>
+
+    <div class="container my-3">
+      <div class="card card-custom">
+        <div class="card-body">
+          <h5 class="card-title">Informasi Pengaduan</h5>
+          <hr>
+          <div class="row">
+            <div class="col-md-6 mt-3">
+              <p class="card-text mb-1"><strong>Lokasi</strong></p>
+              <hr>
+              <p>Jl. Sudirman No. 123, Jakarta Pusat</p>
+              <div class="col-md-6 mt-5">
+                <img src="https://via.placeholder.com/500x350.png" alt="Foto pengaduan" class="img-fluid">
+              </div>
+              <p class="card-text mb-1 mt-4"><strong>Permasalahan</strong></p>
+              <hr>
+              <p>Sampah menumpuk di pinggir jalan</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <nav class="navbar-dark fixed-bottom my-navbar my-3">
+        <div class="container-fluid text-center">
+          <button class="btn custom-btn" type="button">Setujui</button>
+        </div>
+      </nav>
+  </body>
+</html>
