75e69d8dc89a2becd40729b3b2aa86c819e0ba62
dimas <dimas88@mhs.unsyiah.ac.id>
2023-05-15 03:47:21 +0700

    landing page

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@200&family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <title>SortMyTrash</title>
+    <link rel="icon" href="assets/logo Transparan.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="container ">
+            <a class="navbar-brand " href="">
+                <img src="assets/logo Transparan.png" draggable="false" alt="SortMyTrash"height="45">
+                <h2 class="text-center mx-auto">SortMyTrash</h2>
+            </a>
+        </div>
+    </nav>
+    <div class="gambar text-center py-4">
+        <img src="assets/logo Transparan.png" draggable="false" width="246" height="246">
+        <p class="px-5">Halo, Selamat Datang di Aplikasi SortMyTrash<p></p></p>
+    </div>
+
+    <div class="tombol text-center  index">
+  
+        <a href="Halaman login/index.php"><button type="button" class="btn-user">Mulai</button></a><br>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+</html>
