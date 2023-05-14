de4ddda37a5603fe05adf3181aa2907f1bae1c71
dimas <dimas88@mhs.unsyiah.ac.id>
2023-05-15 03:37:19 +0700

    beranda petugas

---
+++ Halaman beranda petugas/beranda petugas.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Sortmytrash</title>
+<link rel="icon" href="../assets/logo Transparan.png" type="image/x-icon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+    />
+    <link rel="stylesheet" href="beranda petugas.css" />
+  </head>
+  <body>
+    <nav>
+      <div class="navbar" style="background-color: #bdb76b">
+      <a class="navbar-brand ms-2" href="../profil petugas/profil.php">
+<i class="bi bi-person-circle fs-3" ></i>
+</a>
+        </a>
+        <span style="color: black" class="mx-auto">Menu Utama</span>
+      </div>
+    </nav>
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <p>
+            <img src="logo Transparan.png" width="180px" />
+          </p>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-6 col-sm-12 text-center">
+          <a
+            href="../laporan pengaduan/laporan petugas.php"
+            class="btn btn-lg btn-home mb-3 d-block"
+            ><img src="gambar pengaduan.svg" width="60" alt="" />  Laporan Pengaduan</a
+          >
+
+          <a
+  href="../saran/saran.php"
+  class="btn btn-lg btn-home mb-3 d-block"
+  ><img src="../assets/Artboard 1.png" width="60" alt="" />  Saran Dan Kritik</a
+>
+    </div>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+      crossorigin="anonymous"
+    ></script>
+    <style>
+      @media only screen and (max-width: 768px) {
+        .col-md-6 {
+          width: 100%;
+        }
+      }
+    </style>
+  </body>
+</html>
