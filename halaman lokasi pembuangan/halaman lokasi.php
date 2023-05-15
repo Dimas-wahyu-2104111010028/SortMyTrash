2ccd23785ec9641f3341b68a38a7546436db39a0
M Rizki Akbar <rizki44@mhs.unsyiah.ac.id>
2023-05-15 11:23:56 +0700

    tambah halaman lokasi pembuangan

---
+++ halaman lokasi pembuangan/halaman lokasi.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Sortmytrash</title>
+<link rel="icon" href="../assets/logo Transparan.png" type="image/x-icon" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="halaman lokasi.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
+</head>
+
+<body>
+  <header class="fixed-top">
+    <div class="d-flex align-items-center">
+      <a href="../halaman beranda/beranda.php" class="position-top start">
+        <i class="bi bi-arrow-left fs-1"></i>
+      </a>
+    </div>
+    <h1 class="text-center mx-auto">Lokasi Pembuangan Sampah</h1>
+  </header>
+  <div class="container">
+    <p class="mt-5">Berikut adalah daftar lokasi pembuangan sampah yang dapat Anda gunakan:</p>
+    <ul>
+      <li >TPA Gampong Jawa</li>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.921431383877!2d95.31326087382101!3d5.578632933424106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30403729f37a4ab5%3A0xc290bd70fda082b2!2sTPA%20Gampong%20Jawa!5e0!3m2!1sid!2sid!4v1683739936621!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <li>TPS Gampong Meunasah Papeun</li>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3971.0143177522973!2d95.36157617382092!3d5.564894533562611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304037fa6b6b822d%3A0x2fa9a4eb16335ec9!2sKontainer%20TPS%20Gampong%20Meunasah%20Papeun!5e0!3m2!1sid!2sid!4v1683740030554!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <li>Bank Sampah USK</li>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3971.0001808860543!2d95.36459097382085!3d5.566987633541536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30403772ff9eb16b%3A0x2b1ee1d48b060ac5!2sBank%20Sampah%20USK!5e0!3m2!1sid!2sid!4v1683740061536!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <li>Pengumpulan Sampah Bawah Jembatan Lamnyong</li>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.951627045162!2d95.35769837382095!3d5.574170533469129!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304037eb1ec77475%3A0x2b5a6df700a3ef43!2sPengumpulan%20sampah!5e0!3m2!1sid!2sid!4v1683740091169!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <li>TPS 3R Lambung</li>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3971.084823510704!2d95.2902354738208!3d5.554443733667758!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304030be9a661121%3A0x559951571de08522!2sTPS%203R%20Lambung!5e0!3m2!1sid!2sid!4v1683740118108!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <li>Kontainer Sampah Paleuh Blang</li>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3971.5263703275195!2d95.38961837382053!3d5.488543834326765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3040398d12314ddd%3A0x52d68c9f5335b7f3!2sKontainer%20sampah%20Paleuh%20Blang!5e0!3m2!1sid!2sid!4v1683740143165!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <li>TPA Regional Blang Bintang, Aceh Besar</li>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3971.339191231829!2d95.47457647382065!3d5.516575734047303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304045e1c36e5f23%3A0xffe15f12c02374b!2sTPA%20Regional%20Blang%20Bintang%2C%20Aceh%20Besar!5e0!3m2!1sid!2sid!4v1683740169932!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </ul>
+    </div>
+  </div>
+</body>
+</html>
